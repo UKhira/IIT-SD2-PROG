5c68233adf0cec5f8cc414e22dff74318825f1d2
Udith Kavishka <udithmanohara@gmail.com>
2023-05-29 21:27:53 +0530

    to be continued...

---
+++ TUTs/TUT_01/Exercise_04._Menu/src/Menu.java
@@ -0,0 +1,15 @@
+public class Menu{
+    public static void main (String [] args) {
+        
+        for (int askterikCount = 0; askterikCount < 8; askterikCount++) {
+            System.out.print("*");
+        }
+
+        System.out.println("\n* MENU *");
+
+        for (int askterikCount = 0; askterikCount < 8; askterikCount++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    } 
+}
