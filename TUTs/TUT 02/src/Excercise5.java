c71d711461d2b8b4cc828215c5bd0fd728203488
Udith Kavishka <udithmanohara@gmail.com>
2023-06-10 14:21:49 +0530

    Finalized

--- TUTs/TUT 02/src/Excercise5.java
+++ TUTs/TUT 02/src/Excercise5.java
@@ -9,18 +9,17 @@ public class Excercise5 {
         int days = 0;
         switch (month) {
             case "January":  days = 31; break;
-
-            case "February": 
-            System.out.println("Enter year : ");
-            int year = monthInput.nextInt();
-            int test = year % 4;
-            System.out.println(test);
-            switch(test) {
-                case 0 : days = 29; break;
-                default : days = 28; break;
-            }
+            case "February": days = 28; break;
             case "March" : days = 31; break;
-
+            case "April" : days = 30; break;
+            case "May" : days = 31; break;
+            case "June" : days = 30; break;
+            case "July" :  days = 31; break;
+            case "August" : days = 31; break;
+            case "September" :  days = 30; break;
+            case "October" : days = 31; break;
+            case "November" : days = 30; break;
+            case "December" : days = 31; break;
         }
         System.out.println(days);
     }
---
+++ TUTs/TUT 02/src/Excercise6.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Excercise6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        char letter = input.next().charAt(0);
+
+        if (letter >= 'a' && letter <= 'z') {
+            System.out.println("A lowercase character");
+        }
+        else if (letter >= 'A' && letter <= 'Z') {
+            System.out.println("An uppercase character");
+        }
+    }
+}
